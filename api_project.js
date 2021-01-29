d8d5810b77bcff19a15c6f6d408bc9486f680ffe
serngro@gmail.com <serngro@gmail.com>
2021-01-29 12:14:47 +0700

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "GSAM",
+  "version": "1.0.0",
+  "description": "apiDoc GSAM",
+  "title": "Document API GSAM",
+  "url": "https://sns-dev-api.gemiso.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-01-29T04:25:15.755Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
